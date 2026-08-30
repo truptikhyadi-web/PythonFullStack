48b53d4b68595ebd10eda902d4094682b32de25b
truptikhyadi-web <truptikhyadi@gmail.com>
2026-08-30 22:37:46 +0530

    Add files via upload

---
+++ positional.py
@@ -0,0 +1,18 @@
+#positional Arguments
+def add(a,b):
+    print("a:",a)
+    print("b:",b)
+    print("sum:",a+b)
+add(a=5,b=5)
+
+
+#Keyword Arguments
+def add(a,b):
+    print("a",a)
+    print("b",b)
+    print(a+b)
+add("2","3")
+add(a=2,b=4)
+    
+
+
