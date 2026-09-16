34e58ba24ecc301e755e280f38c567a43b3cfd83
truptikhyadi-web <truptikhyadi@gmail.com>
2026-09-16 19:43:51 +0530

    Add files via upload

---
+++ TicketBooking.py
@@ -0,0 +1,5 @@
+def book_ticket(name,seat="Not Assigned",class_type="Economy"):
+    print("name:",name)
+    print("seat:",seat)
+    print("class_type:",class_type)
+book_ticket("trupti",522,"Non-AC")
