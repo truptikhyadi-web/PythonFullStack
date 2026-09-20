656abb461db2de554da1cdf9ac6a3a1a607ffb22
truptikhyadi-web <truptikhyadi@gmail.com>
2026-09-20 19:02:58 +0530

    Add files via upload

---
+++ OOPs.py
@@ -0,0 +1,8 @@
+class employee:
+    def project(self):
+        print("projects are completed")
+    def Presentation(self):
+        print("Ready to present the presentstion")
+Emp1=employee()
+Emp1.project()
+Emp1.Presentation()
